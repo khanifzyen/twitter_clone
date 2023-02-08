65bc7d010b6627721b5b1c0d71ca55389490abc5
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2023-02-08 15:45:59 +0700

    login ui textfield

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
+import '../../../constants/constants.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +10,31 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final appBar = UIConstants.appBar();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                AuthField(controller: emailController, hintText: 'Email'),
+                const SizedBox(height: 25.0),
+                AuthField(controller: passwordController, hintText: 'Password'),
+                const SizedBox(height: 25.0),
+                //Button(),
+                //TextSpan()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
